b0d7180db55594e01925ce0782fbae8688211eae
Jonathan Killen <Productionmax@hotmail.com>
2022-08-05 14:10:08 +0800

    Operator function was made with if statements

---
+++ js.js
@@ -0,0 +1,52 @@
+function add (add1,add2) {
+    let addition = add1 + add2;
+    return addition;
+};
+
+function subtract (sub1,sub2) {
+    let subtraction = sub1 - sub2;
+    return subtraction;
+};
+
+function sum(sumArr) {
+    let total = 0;
+    for (let i=0;i<sumArr.length;i++){
+        total += sumArr[i];
+    }
+    return total;
+};
+
+function multiply(multiplyArr) {
+    let product = 1;
+    for (let i=0;i<multiplyArr.length;i++){
+        product = product * multiplyArr[i];
+    } 
+    return product;
+};
+
+function power(lower,indices) {
+    let exponential = lower ** indices;
+    return exponential;
+};
+
+function factorial(num) {
+    if (num === 0 || num ===1){
+        return 1;
+    } 
+    for (let i=num-1;i>=1;i--){
+        num = num * i;
+    }
+    return num;
+};
+
+function operate(num1,num2,operator){
+    if(operator=="+"){
+        return add(num1,num2);
+    } else if (operator=="-"){
+        return subtract(num1,num2);
+    } else if (operator=="X"){
+        return multiply(num1,num2);
+    } else if (operator=="/" ){
+        return divide(num1,num2);
+    }
+}
